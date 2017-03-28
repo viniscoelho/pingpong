9a008958c78a6aced1b979f7b8ba9ffd3b084e97
viniciusmalloc <vini.martins.7@gmail.com>
2017-03-28 16:05:54 -0300

    First commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class pongMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(pongMain))
+        Me.compScoreDraw = New System.Windows.Forms.Label()
+        Me.plrScoreDraw = New System.Windows.Forms.Label()
+        Me.gameTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.gameBall = New System.Windows.Forms.PictureBox()
+        Me.paddleComputer = New System.Windows.Forms.PictureBox()
+        Me.paddlePlayer = New System.Windows.Forms.PictureBox()
+        CType(Me.gameBall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.paddleComputer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.paddlePlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'compScoreDraw
+        '
+        Me.compScoreDraw.AutoSize = True
+        Me.compScoreDraw.BackColor = System.Drawing.Color.Transparent
+        Me.compScoreDraw.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.compScoreDraw.ForeColor = System.Drawing.Color.OrangeRed
+        Me.compScoreDraw.Location = New System.Drawing.Point(245, 31)
+        Me.compScoreDraw.Name = "compScoreDraw"
+        Me.compScoreDraw.Size = New System.Drawing.Size(66, 46)
+        Me.compScoreDraw.TabIndex = 3
+        Me.compScoreDraw.Text = "00"
+        '
+        'plrScoreDraw
+        '
+        Me.plrScoreDraw.AutoSize = True
+        Me.plrScoreDraw.BackColor = System.Drawing.Color.Transparent
+        Me.plrScoreDraw.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.plrScoreDraw.ForeColor = System.Drawing.Color.LimeGreen
+        Me.plrScoreDraw.Location = New System.Drawing.Point(326, 31)
+        Me.plrScoreDraw.Name = "plrScoreDraw"
+        Me.plrScoreDraw.Size = New System.Drawing.Size(66, 46)
+        Me.plrScoreDraw.TabIndex = 4
+        Me.plrScoreDraw.Text = "00"
+        '
+        'gameTimer
+        '
+        Me.gameTimer.Enabled = True
+        Me.gameTimer.Interval = 25
+        '
+        'gameBall
+        '
+        Me.gameBall.BackColor = System.Drawing.Color.Transparent
+        Me.gameBall.BackgroundImage = CType(resources.GetObject("gameBall.BackgroundImage"), System.Drawing.Image)
+        Me.gameBall.Location = New System.Drawing.Point(290, 181)
+        Me.gameBall.Name = "gameBall"
+        Me.gameBall.Size = New System.Drawing.Size(60, 60)
+        Me.gameBall.TabIndex = 2
+        Me.gameBall.TabStop = False
+        '
+        'paddleComputer
+        '
+        Me.paddleComputer.BackColor = System.Drawing.Color.Gold
+        Me.paddleComputer.Location = New System.Drawing.Point(40, 160)
+        Me.paddleComputer.Name = "paddleComputer"
+        Me.paddleComputer.Size = New System.Drawing.Size(20, 128)
+        Me.paddleComputer.TabIndex = 1
+        Me.paddleComputer.TabStop = False
+        '
+        'paddlePlayer
+        '
+        Me.paddlePlayer.BackColor = System.Drawing.Color.Purple
+        Me.paddlePlayer.Location = New System.Drawing.Point(570, 160)
+        Me.paddlePlayer.Name = "paddlePlayer"
+        Me.paddlePlayer.Size = New System.Drawing.Size(20, 128)
+        Me.paddlePlayer.TabIndex = 0
+        Me.paddlePlayer.TabStop = False
+        '
+        'pongMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(624, 442)
+        Me.Controls.Add(Me.plrScoreDraw)
+        Me.Controls.Add(Me.compScoreDraw)
+        Me.Controls.Add(Me.gameBall)
+        Me.Controls.Add(Me.paddleComputer)
+        Me.Controls.Add(Me.paddlePlayer)
+        Me.Name = "pongMain"
+        Me.Text = "Pong Game"
+        CType(Me.gameBall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.paddleComputer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.paddlePlayer, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents paddlePlayer As System.Windows.Forms.PictureBox
+    Friend WithEvents paddleComputer As System.Windows.Forms.PictureBox
+    Friend WithEvents gameBall As System.Windows.Forms.PictureBox
+    Friend WithEvents compScoreDraw As System.Windows.Forms.Label
+    Friend WithEvents plrScoreDraw As System.Windows.Forms.Label
+    Friend WithEvents gameTimer As System.Windows.Forms.Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,155 @@
+﻿Public Class pongMain
+
+#Region "Globals"
+    Dim speed As Single = 17 ' Ball Speed
+    Dim rndInst As New Random() ' Random instance
+    Dim xVel As Single = Math.Cos(rndInst.Next(5, 10)) * speed
+    Dim yVel As Single = Math.Sin(rndInst.Next(5, 10)) * speed
+
+    ' The player's scores.
+    Dim compScore As Integer = 0
+    Dim plrScore As Integer = 0
+
+#End Region
+
+    '#Region "Move the paddle according to the mouse"
+    ' Move the paddle according to the mouse position.
+    'Private Sub pongMain_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+    'If e.Y > 5 And e.Y < Me.Height - 40 - paddlePlayer.Height Then _
+    'paddlePlayer.Location = New Point(paddlePlayer.Location.X, e.Y)
+    'End Sub
+    '#End Region
+
+#Region "Main Timer"
+    Private Sub gameTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles gameTimer.Tick
+        If (compScore = 15 Or plrScore = 15) Then
+            gameTimer.Stop()
+            PlayWin()
+            Dim msg As String = ""
+            If compScore = 15 Then
+                msg &= "Player 1 won!"
+            Else
+                msg &= "Player 2 won!"
+            End If
+            Dim result As Integer = MsgBox("Do you want to keep playing?", MsgBoxStyle.YesNo, msg)
+            If result = DialogResult.No Then
+                End
+            ElseIf result = DialogResult.Yes Then
+                Application.Restart()
+            End If
+        Else
+            'Set the computer player to move according to the ball's position."
+            'If gameBall.Location.Y > 5 And gameBall.Location.Y < Me.Height - 40 Then _
+            '- paddlePlayer.Height Then _
+            'paddleComputer.Location = New Point(paddleComputer.Location.X, gameBall.Location.Y)
+
+            ' Move the game ball.
+            gameBall.Location = New Point(gameBall.Location.X + xVel, gameBall.Location.Y + yVel)
+
+            ' Check for top wall.
+            If gameBall.Location.Y < 0 Then
+                gameBall.Location = New Point(gameBall.Location.X, 0)
+                yVel = -yVel
+            End If
+
+            ' Check for bottom wall.
+            If gameBall.Location.Y > Me.Height - gameBall.Size.Height - 45 Then
+                gameBall.Location = New Point(gameBall.Location.X, Me.Height - gameBall.Size.Height - 45)
+                yVel = -yVel
+            End If
+
+            ' Check for left wall. If passes the paddle, add +1 to player 1.
+            If gameBall.Location.X < 0 Then
+                plrScore += 1
+                gameBall.Location = New Point(Me.Size.Width / 2, Me.Size.Height / 2)
+                plrScoreDraw.Text = Convert.ToString(plrScore)
+                If (plrScore < 10) Then
+                    plrScoreDraw.Text = "0" & plrScoreDraw.Text
+                End If
+                PlayBallOut()
+            End If
+
+            ' Check for right wall. If passes the paddle, add +1 to player 2.
+            If gameBall.Location.X > Me.Width - gameBall.Size.Width - paddlePlayer.Width Then
+                compScore += 1
+                gameBall.Location = New Point(Me.Size.Width / 2, Me.Size.Height / 2)
+                compScoreDraw.Text = Convert.ToString(compScore)
+                If (compScore < 10) Then
+                    compScoreDraw.Text = "0" & compScoreDraw.Text
+                End If
+                PlayBallOut()
+            End If
+
+            ' Check for player paddle.
+            If gameBall.Bounds.IntersectsWith(paddlePlayer.Bounds) Then
+                gameBall.Location = New Point(paddlePlayer.Location.X - gameBall.Size.Width, _
+                gameBall.Location.Y)
+                xVel = -xVel
+                PlayBallPong()
+            End If
+
+            ' Check for computer paddle.
+            If gameBall.Bounds.IntersectsWith(paddleComputer.Bounds) Then
+                gameBall.Location = New Point(paddleComputer.Location.X + paddleComputer.Size.Width + 1, _
+                gameBall.Location.Y)
+                xVel = -xVel
+                PlayBallPong()
+            End If
+        End If
+    End Sub
+#End Region
+
+#Region "Play Sounds"
+    Public Sub PlayBallOut()
+        My.Computer.Audio.Play(My.Resources.ballout, _
+            AudioPlayMode.Background)
+    End Sub
+
+    Public Sub PlayBallPong()
+        My.Computer.Audio.Play(My.Resources.ballpong, _
+            AudioPlayMode.Background)
+    End Sub
+
+    Public Sub PlayWin()
+        My.Computer.Audio.Play(My.Resources.wincheer, _
+            AudioPlayMode.Background)
+    End Sub
+#End Region
+
+#Region "KeyCommands"
+    Private Sub computeKey(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        'For Q press
+        If e.KeyCode = Keys.Q Then
+            Dim newPos As Integer = paddleComputer.Location.Y - 30
+            If newPos > 5 And newPos < Me.Height - 40 - paddleComputer.Height Then
+                paddleComputer.Location = New Point(paddleComputer.Location.X, newPos)
+            End If
+        End If
+
+        'For A press
+        If e.KeyCode = Keys.A Then
+            Dim newPos As Integer = paddleComputer.Location.Y + 30
+            If newPos > 5 And newPos < Me.Height - 40 - paddleComputer.Height Then
+                paddleComputer.Location = New Point(paddleComputer.Location.X, newPos)
+            End If
+        End If
+
+        'For P press
+        If e.KeyCode = Keys.P Then
+            Dim newPos As Integer = paddlePlayer.Location.Y - 30
+            If newPos > 5 And newPos < Me.Height - 40 - paddlePlayer.Height Then
+                paddlePlayer.Location = New Point(paddlePlayer.Location.X, newPos)
+            End If
+        End If
+
+        'For L press
+        If e.KeyCode = Keys.L Then
+            Dim newPos As Integer = paddlePlayer.Location.Y + 30
+            If newPos > 5 And newPos < Me.Height - 40 - paddlePlayer.Height Then
+                paddlePlayer.Location = New Point(paddlePlayer.Location.X, newPos)
+            End If
+        End If
+    End Sub
+#End Region
+
+End Class
